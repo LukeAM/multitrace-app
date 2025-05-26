c9336e16d24dd6a5919974c4d184da9588a45cc6
LukeAM <lucaamiller@gmail.com>
2025-05-26 18:54:35 +0200

    more

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { auth } from "@clerk/nextjs";
+import { auth } from '@clerk/nextjs/server';
 import { redirect } from "next/navigation";
 import ClientPage from "./client-page";
 
 export default async function Page() {
-  const { userId } = auth();
+  const { userId } = await auth();
   
   if (!userId) {
     redirect("/sign-in");

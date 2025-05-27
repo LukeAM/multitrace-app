f70ff5e96bacbadf35fdabf14ca4c495bd708997
LukeAM <lucaamiller@gmail.com>
2025-05-27 19:09:29 +0200

    more

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
-// middleware.ts
-import { authMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/sign-in", "/sign-up", "/sso-callback", "/debug-supabase", "/favicon.ico"],
 });
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!_next|.*\\..*).*)"],
 };

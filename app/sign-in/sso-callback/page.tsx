195b82b822e3762290920ebfa5e334e5a3391134
LukeAM <lucaamiller@gmail.com>
2025-05-27 16:07:14 +0200

    more

--- app/sign-in/sso-callback/page.tsx
+++ app/sign-in/sso-callback/page.tsx
@@ -1,5 +1,14 @@
-import { SSOCallback } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function SSOCallbackPage() {
-  return <SSOCallback />;
+export default async function SSOCallbackPage() {
+  // This triggers the session to be validated, and redirects home
+  const { userId } = auth();
+
+  if (userId) {
+    redirect("/");
+  }
+
+  // Optionally render a loading state
+  return <p>Signing you in...</p>;
 }
